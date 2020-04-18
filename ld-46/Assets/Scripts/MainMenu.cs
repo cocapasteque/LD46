0c8d6bb435386f9ed48b0ac63c103708e8eb1750
a.thiry <a.thiry@str8labs.com>
2020-04-18 08:09:15 +0200

    A working exit button for once in ldjam history

--- ld-46/Assets/Scripts/MainMenu.cs
+++ ld-46/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
