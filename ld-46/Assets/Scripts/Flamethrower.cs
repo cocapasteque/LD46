ece392b3e3e7894cfd8a2553fb0805d169e99653
a.thiry <a.thiry@str8labs.com>
2020-04-18 19:32:53 +0200

    Added flamethrower follow logic

---
+++ ld-46/Assets/Scripts/Flamethrower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    public Transform target;
+    public Transform rotationOrigin;
+
+    public float fireRate;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtTarget();
+    }
+
+    void LookAtTarget()
+    {
+        float rad = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x);
+        // Get Angle in Degrees
+        float deg = (180 / Mathf.PI) * rad;
+        // Rotate Object
+        rotationOrigin.rotation = Quaternion.Euler(0, 0, deg);
+    }
+}
