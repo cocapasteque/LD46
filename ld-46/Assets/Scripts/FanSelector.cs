9c1eddff16dad7e0d6744b5c21f33f70ca7aacd1
Dalon <s.laimer@gmx.de>
2020-04-18 20:42:15 +0200

    new fan ui

--- ld-46/Assets/Scripts/FanSelector.cs
+++ ld-46/Assets/Scripts/FanSelector.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FanSelector : MonoBehaviour
 {
     public bool selected = false;
+    public Fan Fan;
 
     public Color selectedColor;
     public Color deselectedColor;
     public Level level;
     public SpriteRenderer graphics;
 
+    public float currentAngle;
+
+    private void Start()
+    {
+        currentAngle = 0f;
+    }
+
+    private void Update()
+    {
+        if (selected)
+        {
+            currentAngle = GameOverlay.Instance.RotationSlider.normalizedValue;
+            float rad = (1f - GameOverlay.Instance.RotationSlider.normalizedValue) * 2f * Mathf.PI;
+            float deg = (180 / Mathf.PI) * rad;
+            transform.parent.rotation = Quaternion.Euler(0, 0, deg);
+        }
+    }
+
     public void Select()
     {
         selected = true;
+        Fan.Select();
         graphics.color = selectedColor;
+        GameOverlay.Instance.RotationSlider.normalizedValue = currentAngle;
+        GameOverlay.Instance.SelectFan();
         //        tooltipView.Show();
     }
 
@@ -22,6 +45,7 @@ public class FanSelector : MonoBehaviour
     {
         selected = false;
         graphics.color = deselectedColor;
+        GameOverlay.Instance.DeselectFan();
         //        tooltipView.Hide();
     }
 
@@ -38,6 +62,11 @@ public class FanSelector : MonoBehaviour
         float deg = (180 / Mathf.PI) * rad;
         // Rotate Object
         transform.parent.rotation = Quaternion.Euler(0, 0, deg);
+    }
+
+    public void Rotate2()
+    {
+        
     }
 
     public void Remove()
