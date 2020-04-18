1760b514d6658a853d2c113ff67bc2510517a6bc
a.thiry <a.thiry@str8labs.com>
2020-04-18 08:58:20 +0200

    Added hinge joint to player

---
+++ ld-46/Assets/Scripts/Fan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+
+    public float force = 5f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ld-46/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float baseForce = 5f;
+
+    private Rigidbody2D m_rb;
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyBaseForce();
+    }
+
+    void ApplyBaseForce()
+    {
+        m_rb.AddForce(Vector2.up * baseForce);
+    }
+}
