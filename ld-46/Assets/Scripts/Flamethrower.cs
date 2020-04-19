9267b87b972dd464439cfc89e9c35b6f21d9dc68
Dalon <s.laimer@gmx.de>
2020-04-19 04:11:10 +0200

    flamethrower logic

--- ld-46/Assets/Scripts/Flamethrower.cs
+++ ld-46/Assets/Scripts/Flamethrower.cs
@@ -7,12 +7,39 @@ public class Flamethrower : MonoBehaviour
     public Transform target;
     public Transform rotationOrigin;
 
+    public bool trackTarget;
+
+    [Tooltip("Shots per minute")]
     public float fireRate;
-    
+    public float fireDuration;
+    public float firstFireDelay;
+
+    public List<ParticleSystem> ParticleSystems;
+
+    [Tooltip("Offset: x,y | Size: z,w")]
+    public List<Vector4> ColliderSizes;
+
+    public float GrowDuration;
+    public float ShrinkDuration;
+
+    public BoxCollider2D Coll;
+
+    private void Start()
+    {
+        foreach(var particle in ParticleSystems)
+        {
+            particle.Stop(); 
+            var main = particle.main;
+            main.duration = fireDuration;
+        }
+        StartCoroutine(Fire());
+    }
+
     // Update is called once per frame
     void Update()
     {
-        LookAtTarget();
+        if (trackTarget)
+            LookAtTarget();
     }
 
     void LookAtTarget()
@@ -22,5 +49,57 @@ public class Flamethrower : MonoBehaviour
         float deg = (180 / Mathf.PI) * rad;
         // Rotate Object
         rotationOrigin.rotation = Quaternion.Euler(0, 0, deg);
+    }
+
+    private IEnumerator Fire()
+    {
+        yield return new WaitForSeconds(firstFireDelay);
+        while (true)
+        {
+            foreach (var particle in ParticleSystems)
+            {
+                particle.Play();
+                StartCoroutine(GrowCollider());
+            }
+            yield return new WaitForSeconds(fireDuration);
+            StartCoroutine(ShrinkCollider());
+            yield return new WaitForSeconds(60f/fireRate);
+        }
+    }
+
+    private IEnumerator GrowCollider()
+    {
+        float startTime = Time.time;
+        Vector2 fromOffset = new Vector2(ColliderSizes[0].x, ColliderSizes[0].y);
+        Vector2 fromSize = new Vector2(ColliderSizes[0].z, ColliderSizes[0].w);
+
+        Vector2 toOffset = new Vector2(ColliderSizes[1].x, ColliderSizes[1].y);
+        Vector2 toSize = new Vector2(ColliderSizes[1].z, ColliderSizes[1].w);
+
+        while (Time.time - startTime <= GrowDuration)
+        {
+            Coll.offset = Vector2.Lerp(fromOffset, toOffset, (Time.time - startTime) / GrowDuration);
+            Coll.size = Vector2.Lerp(fromSize, toSize, (Time.time - startTime) / GrowDuration);
+            yield return null;
+        }        
+    }
+
+    private IEnumerator ShrinkCollider()
+    {
+        float startTime = Time.time;
+        Vector2 fromOffset = new Vector2(ColliderSizes[1].x, ColliderSizes[1].y);
+        Vector2 fromSize = new Vector2(ColliderSizes[1].z, ColliderSizes[1].w);
+
+        Vector2 toOffset = new Vector2(ColliderSizes[2].x, ColliderSizes[2].y);
+        Vector2 toSize = new Vector2(ColliderSizes[2].z, ColliderSizes[2].w);
+
+        while (Time.time - startTime <= GrowDuration)
+        {
+            Coll.offset = Vector2.Lerp(fromOffset, toOffset, (Time.time - startTime) / GrowDuration);
+            Coll.size = Vector2.Lerp(fromSize, toSize, (Time.time - startTime) / GrowDuration);
+            yield return null;
+        }
+        Coll.offset = new Vector2(ColliderSizes[0].x, ColliderSizes[0].y);
+        Coll.size = new Vector2(ColliderSizes[0].z, ColliderSizes[0].w);
     }
 }
