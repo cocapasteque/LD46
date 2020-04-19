44c7f2a2345287802170462c515fac5a0f5adcf8
a.thiry <a.thiry@str8labs.com>
2020-04-19 19:53:39 +0200

    Not add doubles in finish line

--- ld-46/Assets/Scripts/FinishLine.cs
+++ ld-46/Assets/Scripts/FinishLine.cs
@@ -7,7 +7,7 @@ public class FinishLine : MonoBehaviour
     public string unlockingLevel;
     public GameObject effectPrefab;
     public Transform[] effectOrigin;
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -18,13 +18,18 @@ public class FinishLine : MonoBehaviour
                 effect.transform.localPosition = Vector3.zero;
                 Destroy(effect, 5);
             }
-            
+
             // Unlocking level
             var levelsString = PlayerPrefs.GetString("unlocked_levels", "['Tutorial','Level 1']");
             Debug.Log(levelsString);
             var array = JsonConvert.DeserializeObject<string[]>(levelsString);
             var list = array.ToList();
-            list.Add(unlockingLevel);
+            
+            if (!list.Contains(unlockingLevel))
+            {
+                list.Add(unlockingLevel);
+            }
+
             levelsString = JsonConvert.SerializeObject(list.ToArray());
             Debug.Log(levelsString);
             PlayerPrefs.SetString("unlocked_levels", levelsString);
