3b8157c584832ed47e2120515b5987253326e75e
Dalon <s.laimer@gmx.de>
2020-04-20 20:18:15 +0200

    limited alias to 1-12 characters

--- ld-46/Assets/Scripts/AliasInput.cs
+++ ld-46/Assets/Scripts/AliasInput.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using Doozy.Engine;
+using Doozy.Engine.UI;
 using TMPro;
 using UnityEngine;
 
 public class AliasInput : MonoBehaviour
 {
     public TMP_InputField inputField;
+    public UIButton ConfirmAliasButton;
 
     private string alias;
     // Start is called before the first frame update
@@ -38,5 +40,17 @@ public class AliasInput : MonoBehaviour
         PlayerPrefs.SetString("player_alias", inputField.text);
         Debug.Log("Setting alias to " + inputField.text);
         PlayerPrefs.Save();
+    }
+
+    public void CheckAlias()
+    {
+        if (inputField.text.Length <= 0 || inputField.text.Length > 12)
+        {
+            ConfirmAliasButton.DisableButton();
+        }
+        else
+        {
+            ConfirmAliasButton.EnableButton();
+        }
     }
 }
